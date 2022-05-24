df4a82a643d373aefb601e1b24771d357e7acc66
ravi maithrey <maitrey.ind@gmail.com>
2022-05-24 21:39:09 +0530

    from my pc

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import argparse
-from tokenize import String
+
 
 parser = argparse.ArgumentParser(description="TODO list manager")
-parser.add_argument("todo", "-t", "--todo", metavar="todo: ", type=String, help="Write an item to add to your TODO list")
-input = parser.parse_args()
+parser.add_argument(
+    "todo", metavar="todo: ", type=str, help="Write an item to add to your TODO list",
+)
+parser.add_argument(
+    "view",
+    metavar="view todo list, first word is unique identifier",
+    help="This takes no arguments, don't give",
+)
 
+arg = parser.parse_args()
+if len(arg.todo) != 0:
+    file = open("/home/ravi/Documents/TODO/TODO.txt", "a")
+    file.write(arg.todo)
+    file.close()
