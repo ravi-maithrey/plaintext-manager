2f9752e06eea7280197634c0744d27b38fdc4a37
ravi maithrey <maitrey.ind@gmail.com>
2022-06-01 20:30:28 +0530

    from my pc

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import argparse
+import uuid
 
 
 parser = argparse.ArgumentParser(description="TODO list manager")
 parser.add_argument(
-    "todo", metavar="todo: ", type=str, help="Write an item to add to your TODO list",
+    "--todo", "-t", metavar="todo: ", type=str, help="Write an item to add to your TODO list",
 )
 parser.add_argument(
-    "view",
-    metavar="view todo list, first word is unique identifier",
+    "--view", "-v",
+    metavar="view: ",
     help="This takes no arguments, don't give",
 )
 
 arg = parser.parse_args()
 if len(arg.todo) != 0:
+    full_unique_id = str(uuid.uuid4())
+    unique_id = full_unique_id[0:8]
+    string_to_write = unique_id + " " + arg.todo + "\n"
     file = open("/home/ravi/Documents/TODO/TODO.txt", "a")
-    file.write(arg.todo)
+    file.write(string_to_write)
     file.close()
